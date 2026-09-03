425f8985246ca9887839da27a5d9735067cf6d31
Eman Sajjad <akzachoudhry1123@gmail.com>
2026-09-03 21:17:37 +0500

    my user api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import type { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import myUserRoute from "./routes/MyUserRoute.js";
 
 
 mongoose
@@ -20,6 +21,8 @@ app.get("/", (req, res) => {
 app.get("/test", async(req: Request, res: Response)=> {
      res.json({message: "Hi"})
 })
+
+app.use("/api/my/user", myUserRoute);
 
 app.listen(7000, ()=> {
     console.log("sever started on localhost: 7000")
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,13 +4,24 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import AppRoutes from './AppRoutes.tsx'
 import Auth0ProviderWithNavigate from './auth/Auth0ProviderWithNavigate.tsx'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
+     <QueryClientProvider client={queryClient}>
     <Auth0ProviderWithNavigate>
           <AppRoutes />
     </Auth0ProviderWithNavigate>
+    </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>,
 )
